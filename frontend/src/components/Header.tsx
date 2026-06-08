e1065bd7dba5840c406bf3ae3a135149c3a326e6
rayzera <rayzera000@gmail.com>
2026-06-08 09:03:38 +0000

    edit header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="flex justify-between items-center">
-      <h1 className="text-3xl font-bold text-slate-800">Kiting Capacity</h1>
+      <h1 className="text-3xl font-bold text-slate-800">Kitting Capacity Calculation</h1>
     </div>
   );
 }
